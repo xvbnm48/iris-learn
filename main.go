dbb5446a72b6448265faa31b4b7436d65953f5ac
fariz <fariz.wisnu@bluebirdgroup.com>
2023-01-13 16:25:01 +0700

    init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := iris.Default()
+	app.Use(myMiddleware)
+
+	app.Handle("GET", "/ping", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "pong"})
+	})
+
+	// Listens and serves incoming http requests
+	// on http://localhost:8080.
+	app.Listen(":8080")
+}
+
+func myMiddleware(ctx iris.Context) {
+	ctx.Application().Logger().Infof("Runs before %s", ctx.Path())
+	ctx.Next()
+}
